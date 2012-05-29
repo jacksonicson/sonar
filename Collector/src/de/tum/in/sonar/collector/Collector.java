a7a778f5b52e830a13e9e832bd231a98f6534b07
Andreas Wolke <jacksonicson@gmail.com>
2012-05-29 13:29:27 +0200

    A simple test client which creates a new table in HBase, writes and reads data from it.

--- Collector/src/de/tum/in/sonar/collector/Collector.java
+++ Collector/src/de/tum/in/sonar/collector/Collector.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
@@ -19,22 +20,46 @@ public class Collector {
 		// Server: 192.168.110.108
 
 		Configuration config = HBaseConfiguration.create();
-		config.set("hbase.zookeeper.quorum", "hadoop1");
 
-		HTable testTable = new HTable(config, "test");
+		/*
+		 * HBaseAdmin hbase = new HBaseAdmin(config); HTableDescriptor desc =
+		 * new HTableDescriptor("TEST");
+		 * 
+		 * HColumnDescriptor meta = new
+		 * HColumnDescriptor("personal".getBytes());
+		 * 
+		 * HColumnDescriptor prefix = new
+		 * HColumnDescriptor("account".getBytes());
+		 * 
+		 * desc.addFamily(meta); desc.addFamily(prefix);
+		 * 
+		 * hbase.createTable(desc);
+		 */
+
+		System.out.println("New table statement");
+
+		HTable testTable = new HTable(config, "TEST");
+
+		System.out.println("New table statement done ");
 
 		for (int i = 0; i < 100; i++) {
-			byte[] family = Bytes.toBytes("cf");
-			byte[] qual = Bytes.toBytes("a");
+			byte[] family = Bytes.toBytes("personal");
+			byte[] qual = Bytes.toBytes("account");
 
 			Scan scan = new Scan();
 			scan.addColumn(family, qual);
+
 			ResultScanner rs = testTable.getScanner(scan);
+
 			for (Result r = rs.next(); r != null; r = rs.next()) {
 				byte[] valueObj = r.getValue(family, qual);
-				String value = new String(valueObj);
-				System.out.println(value);
+				int value = Bytes.toInt(valueObj); 
+				System.out.println("value: " + value);
 			}
+
+//			Put put = new Put(Bytes.toBytes(i));
+//			put.add(family, qual, Bytes.toBytes(i));
+//			testTable.put(put);
 		}
 
 		testTable.close();
@@ -42,6 +67,7 @@ public class Collector {
 	}
 
 	public static void main(String[] args) {
+		
 		try {
 			new Collector();
 		} catch (IOException e) {
---
+++ Collector/src/de/tum/in/sonar/collector/Server.java
@@ -0,0 +1,5 @@
+package de.tum.in.sonar.collector;
+
+public class Server {
+	
+}
