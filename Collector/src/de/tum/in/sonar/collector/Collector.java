9e9ed6f65408fd67a40bb4afd8345d9467ff5947
Andreas Wolke <jacksonicson@gmail.com>
2012-05-29 12:39:07 +0200

    started a small hbase client in the collector

--- Collector/src/de/tum/in/sonar/collector/Collector.java
+++ Collector/src/de/tum/in/sonar/collector/Collector.java
@@ -1,16 +1,51 @@
 package de.tum.in.sonar.collector;
 
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.util.Bytes;
+
 public class Collector {
 
-	public Collector() {
-		
+	public Collector() throws IOException {
+
 		// Test some HBase stuff
-		
-		
-		
+
+		// Server: 192.168.110.108
+
+		Configuration config = HBaseConfiguration.create();
+		config.set("hbase.zookeeper.quorum", "hadoop1");
+
+		HTable testTable = new HTable(config, "test");
+
+		for (int i = 0; i < 100; i++) {
+			byte[] family = Bytes.toBytes("cf");
+			byte[] qual = Bytes.toBytes("a");
+
+			Scan scan = new Scan();
+			scan.addColumn(family, qual);
+			ResultScanner rs = testTable.getScanner(scan);
+			for (Result r = rs.next(); r != null; r = rs.next()) {
+				byte[] valueObj = r.getValue(family, qual);
+				String value = new String(valueObj);
+				System.out.println(value);
+			}
+		}
+
+		testTable.close();
+
 	}
-	
+
 	public static void main(String[] args) {
-		new Collector(); 
+		try {
+			new Collector();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
