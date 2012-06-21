10a9c7369b4dd87130fd2caefe3abb2a1decfef6
Andreas Wolke <jacksonicson@gmail.com>
2012-06-21 22:11:31 +0200

    network sensor

---
+++ sensors/psutilnet/main.py
@@ -0,0 +1,41 @@
+import psutil
+import sys
+import time
+
+NAME = 'psutilnet'
+
+last_bytes_sent = 0
+last_bytes_recv = 0
+
+while True:
+    iostat = psutil.network_io_counters()
+    bytes_sent = iostat.bytes_sent
+    if last_bytes_sent == 0:
+        last_bytes_sent = bytes_sent
+    
+    bytes_recv = iostat.bytes_recv
+    if last_bytes_recv == 0:
+        last_bytes_recv = bytes_recv
+    
+    
+    delta_bytes_sent = bytes_sent - last_bytes_sent
+    delta_bytes_recv = bytes_recv - last_bytes_recv
+    last_bytes_recv = bytes_recv
+    last_bytes_sent = bytes_sent
+
+    line = ''
+    line += str(time.time()) + ','
+    line += NAME + '.recv' + ','
+    value = str(delta_bytes_recv)
+    line += value
+    print (line)
+
+    line = ''
+    line += str(time.time()) + ','
+    line += NAME + '.sent' + ','
+    value = str(delta_bytes_sent)
+    line += value
+    print (line)
+    
+    sys.stdout.flush()
+    time.sleep(3)
