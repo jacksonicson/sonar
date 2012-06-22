f67ddb65850cf33513cda18a85741b0c587a85e3
Andreas Wolke <jacksonicson@gmail.com>
2012-06-22 18:14:07 +0200

    source code for configuring the sensors using yaml files.

--- sensors/build.py
+++ sensors/build.py
@@ -2,6 +2,40 @@ import os
 import shutil
 import string
 import cli
+import yaml
+from optparse import OptionParser
+from collector import CollectService, ManagementService, ttypes
+from thrift import Thrift
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TSocket, TTransport
+
+def configure(sensor, path):
+    conf = os.path.join(path, 'conf.yaml')
+    if os.path.exists(conf):
+        yf = open(conf, 'r')
+        content = yaml.load(yf)
+        yf.close()
+        
+        interval = 0
+        if hasattr(content, 'interval'):
+            interval = int(content.interval)
+        
+        transport = TSocket.TSocket('131.159.41.171', 7931)
+        transport = TTransport.TBufferedTransport(transport)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = ManagementService.Client(protocol);
+        transport.open();
+        
+        config = ttypes.SensorConfiguration()
+        config.interval = interval
+        client.setSensorConfiguration(sensor, config)
+        
+        transport.close()
+        print 'configuration applied'
+        
+        print content
+        
+    pass
 
 def createSensor(name, path):
     print 'creating zip...'
@@ -33,6 +67,7 @@ def main():
         package = createSensor(subdir, sensorPath)
         package = os.path.join(path, package)
         deploy(subdir, package)
+        configure(subdir, sensorPath)
         
 if __name__ == '__main__':
     main() 
