c4409d4876a737481ba26f151ba0b2ba1af2ea48
Andreas Wolke <jacksonicson@gmail.com>
2012-07-18 13:19:17 +0200

    testing async server

--- Relay/relay.py
+++ Relay/relay.py
@@ -1,10 +1,44 @@
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+from thrift.server import TNonblockingServer
+from relay import *
 
+# sys.path.append('../gen-py')
 
-
-
+class RelayHandler(object):
+    def __init__(self):
+        pass
+    
+    
+    def execute(self, code):
+        print 'executing'
+        pass
+    
+    def getPids(self):
+        pass
+    
+    def kill(self, pid):
+        pass
+    
+    
 def main():
-    print 'main started'
-
+    print 'ok'
+    
+    handler = RelayHandler()
+    
+    processor = RelayService.Processor(handler)
+    transport = TSocket.TServerSocket(port=9191)
+    
+    # tfactory = TTransport.TBufferedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+    
+    
+    server = TNonblockingServer.TNonblockingServer(processor,  transport)
+    
+    server.serve()
+    
 
 if __name__ == "__main__":
     main()
