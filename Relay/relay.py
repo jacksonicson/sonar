9d8d285381107f744057cebc6471125c7f0eca04
Andreas Wolke <jacksonicson@gmail.com>
2012-07-18 12:34:39 +0200

    relay thrift sources

---
+++ Relay/relay.py
@@ -0,0 +1,10 @@
+
+
+
+
+def main():
+    print 'main started'
+
+
+if __name__ == "__main__":
+    main()
--- Thrift/relay.thrift
+++ Thrift/relay.thrift
@@ -5,5 +5,9 @@ typedef i32 int
 
 service RelayService {
 
+	void execute(1:string code);
+
+	list<int> getPids();
 	
+	void kill(1:int pid); 
 }
