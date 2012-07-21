af20faac42b6945ff1b3ae5fc029e34b1a6e4ba0
Andreas Wolke <jacksonicson@gmail.com>
2012-07-21 23:48:28 +0200

    relaycontrol starts the rain driver now!

--- RelayControl/rain_start/main.sh
+++ RelayControl/rain_start/main.sh
@@ -2,7 +2,7 @@
 
 # Update configuration files
 cp *.json /opt/rain/config
-# cd /opt/rain/current/
+cd /opt/rain/
 
 # Lauch Rain
-# python start.py driver config/rain.config.specj.json
+python start.py driver config/rain.config.specj.json 1>&2 rain.log &
