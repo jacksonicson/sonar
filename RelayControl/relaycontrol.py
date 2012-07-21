72c6255e91ee4f741049d5e2a4a0a2fe9c6712d8
Andreas Wolke <jacksonicson@gmail.com>
2012-07-21 17:17:10 +0200

    removed comments from relaycontrol

--- RelayControl/relaycontrol.py
+++ RelayControl/relaycontrol.py
@@ -120,44 +120,6 @@ def main():
 
     print 'Starting reactor...'
     reactor.run()
-    
-#    transport = TSocket.TSocket('playdb', 7900)
-#    transport = TTransport.TFramedTransport(transport)
-#    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-#    
-#    client = RelayService.Client(protocol)
-#    transport.open()
-#    
-#    
-#    
-#    
-#    print "Creating ZIPs"
-#    
-#    
-#    # Test the execute routine
-#    str = """for i in range(0,10):
-#        print 'hello %i' % (i)
-#    """
-#    client.execute(str)
-#
-#    # Read contents of the zip file
-#    file = open('spec_dbload.zip', 'rb')
-#    data = file.read()
-#    file.close()
-#    
-#    # Launch the ZIP file
-##    res = client.launch(data, "test")
-##    print res
-#    
-#    pid = client.launchNoWait(data, "test")
-#    print 'pid is %i' % (pid)
-#    
-#    while True:
-#        sta = client.isAlive(pid)
-#        print 'alive %i' % (sta)
-#        time.sleep(1)
-    
-
 
 if __name__ == '__main__':
     main()
