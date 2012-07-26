5b38a8b39904406512bd1f528f252469483f3cc1
Andreas Wolke <jacksonicson@gmail.com>
2012-07-26 17:38:18 +0200

    upated profiles for rain

--- RelayControl/control/relaycontrol.py
+++ RelayControl/control/relaycontrol.py
@@ -169,7 +169,7 @@ def main():
     wait = defer.DeferredList(dlist)
     
     # Decide what to do after connection setup
-    start = True
+    start = False
     if start:
         print 'starting system ...'
         wait.addCallback(start_phase)
