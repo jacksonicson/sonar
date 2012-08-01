72a79d5cf8fa867d9b6a78d0d068e95140791d40
Andreas Wolke <jacksonicson@gmail.com>
2012-08-01 23:03:02 +0200

    started implementing the relay code for a VM setup process

--- RelayControl/virtual/vmsetup.py
+++ RelayControl/virtual/vmsetup.py
@@ -1,8 +1,35 @@
+from control import drones, hosts
+from datetime import datetime
 from lxml import etree
+from rain import RainService, constants, ttypes
+from relay import RelayService
+from thrift import Thrift, Thrift
+from thrift.protocol import TBinaryProtocol, TBinaryProtocol
+from thrift.transport import TSocket, TTransport, TTwisted
+from twisted.internet import defer, reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.protocol import ClientCreator
 import libvirt
 
+DEFAULT_SETUP_IP = ''
+RELAY_PORT = 7900
+
+conn = None
+
+
 def setup(vm):
-    pass
+        
+    # Spin (blocking) until relay is up and running
+    creator = ClientCreator(reactor,
+                          TTwisted.ThriftClientProtocol,
+                          RelayService.Client,
+                          TBinaryProtocol.TBinaryProtocolFactory(),
+                          ).connectTCP(DEFAULT_SETUP_IP, RELAY_PORT)
+    creator.addCallback(lambda conn: conn.client)
+    
+    d = defer.Deferred()
+    creator.addCallback(d.callback)
+    
 
 
 def clone(conn, source, target):
@@ -28,7 +55,7 @@ def clone(conn, source, target):
     #print xml_vol_desc
     
     # Create a new volume
-    new_volume = pool.createXMLFrom(xml_vol_desc, volume, 0)
+    # new_volume = pool.createXMLFrom(xml_vol_desc, volume, 0)
     
     # Reconfigure the domain
     xml_domain_desc = domain.XMLDesc(0)
@@ -36,6 +63,9 @@ def clone(conn, source, target):
     xml_tree = etree.fromstring(xml_domain_desc)
     name = xml_tree.xpath('/domain/name')[0]
     name.text = target
+    
+    uuid = xml_tree.xpath('/domain/uuid')[0]
+    uuid.getparent().remove(uuid)
     
     source = xml_tree.xpath('/domain/devices/disk/source')[0]
     source.set('file', '/mnt/s0a0/' + target + '.img')
@@ -47,20 +77,26 @@ def clone(conn, source, target):
     print xml_domain_desc
     
     # Create a new Domain
-    conn.createXML(xml_domain_desc, 0)
-         
+    conn.defineXML(xml_domain_desc)
+   
+   
+count = 0
+def next_vm():
+    clone(conn, 'jacksch', 'test0')
+    setup('test0')
+    
+    count += 1
+    if count > 0:
+        print 'exiting...'
+        conn.close()      
 
 
 def main():
     print 'connecting'
     conn = libvirt.open("qemu+ssh://root@srv0/system")
-    
-    clone(conn, 'jacksch', 'test0')
-    setup('test0')
-    
-    print 'exiting...'
-    conn.close()
 
+    next_vm()    
+    
 
 if __name__ == '__main__':
     main()
