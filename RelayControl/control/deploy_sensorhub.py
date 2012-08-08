514fc5ddb679b1225f32b121ae4037a4fe7808a7
Andreas Wolke <jacksonicson@gmail.com>
2012-08-08 10:27:56 +0200

    deploy sensorhub (start it) on all of the mysql machines.

--- RelayControl/control/deploy_sensorhub.py
+++ RelayControl/control/deploy_sensorhub.py
@@ -13,7 +13,7 @@ def deploy_phase(client_list):
     # Deploy sensorhubs
     for host in hosts.get_hosts('deploy'):
         print '   %s' % (host)
-        d = base.launch(client_list, host, 'sensorhub_deploy', wait=True)
+        d = base.launch(client_list, host, 'sensorhub_start', wait=True)
         dlist.append(d)
     
     # Wait for all drones to finish and set phase
@@ -25,7 +25,12 @@ def main():
     drones.main()
     
     # Add hosts
-    hosts.add_host('playground', 'deploy')
+    hosts.add_host('mysql0', 'deploy')
+    hosts.add_host('mysql1', 'deploy')
+    hosts.add_host('mysql2', 'deploy')
+    hosts.add_host('mysql3', 'deploy')
+    hosts.add_host('mysql4', 'deploy')
+    hosts.add_host('mysql5', 'deploy')
     
     # Connect
     hosts_map = hosts.get_hosts_list()
