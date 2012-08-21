af515f2c707ea50de824e83345d1921c5a93484e
Andreas Wolke <jacksonicson@gmail.com>
2012-08-21 16:10:53 +0200

    almost running benchmark

--- RelayControl/control/start_benchmark.py
+++ RelayControl/control/start_benchmark.py
@@ -4,35 +4,47 @@ from thrift.protocol import TBinaryProtocol
 from thrift.transport import TTwisted
 from twisted.internet import defer, reactor
 from twisted.internet.protocol import ClientCreator
-import math
 import logic.controller as controller
+import math
 
 def finished(done, client_list):
-    print "execution successful"
+    print 'finish phase'
     reactor.stop()
     
     # Launch the controller
-    controller.main() 
+    print '### CONTROLLER ###############################'
+    print 'launching controller'
+    # controller.main() 
 
 
 def rain_started(ret, rain_client, client_list):
-    print 'rain benchmark started'
+    print 'rain is driving load now'
     finished(ret, client_list)
 
 
-def rain_connected(rain_client, client_list):
-    print 'connected with rain'
-    d = rain_client.startBenchmark(long(base.millis()))
-    d.addCallback(rain_started, rain_client, client_list)
+def rain_connected(rain_clients, client_list):
+    print 'releasing load...'
+    from collections import Iterable
+    if isinstance(rain_clients, Iterable) == False:
+        rain_clients = [rain_clients] 
+    
+    for client in rain_clients:
+        print '   * releasing %s' % (client)
+        d = client.startBenchmark(long(base.millis()))
+    
+    d.addCallback(rain_started, rain_clients, client_list)
     
 
 def trigger_rain_benchmark(ret, client_list):
-    print 'connecting with rain'
-    creator = ClientCreator(reactor,
-                          TTwisted.ThriftClientProtocol,
-                          RainService.Client,
-                          TBinaryProtocol.TBinaryProtocolFactory(),
-                          ).connectTCP('load1', 7852)
+    print 'connecting with rain drivers...'
+    
+    for driver in hosts.get_hosts('load'):
+        print '   * connecting %s' % (driver)
+        creator = ClientCreator(reactor,
+                              TTwisted.ThriftClientProtocol,
+                              RainService.Client,
+                              TBinaryProtocol.TBinaryProtocolFactory(),
+                              ).connectTCP(driver, 7852)
                           
     creator.addCallback(lambda conn: conn.client)
     creator.addCallback(rain_connected, client_list)
@@ -53,26 +65,30 @@ def phase_start_rain(done, client_list):
     
     for i in range(0, driver_count):
         driver = drivers[i]
-        
+        print i
+        print targets_per_driver
+        # Build targets for configuration
         config_targets = []
         for target in targets[i * targets_per_driver : (i + 1) * targets_per_driver]:
             config_target = {}
             config_target['target'] = target
             config_targets.append(config_target)
-        
+
+        print config_targets
+                
         # Configure drone
         drones.prepare_drone('rain_start', 'rain.config.specj.json', targets=config_targets)
-        drones.create_drone('glassfish_configure')
         
-        # Launch drone
+        # Launch this drone
         d = base.wait_for_message(client_list, driver, 'rain_start', 'Waiting for start signal...', '/opt/rain/rain.log')
         dlist.append(d)
     
     
     # Wait for all load drivers to start
     dl = defer.DeferredList(dlist)
-    # dl.addCallback(trigger_rain_benchmark, client_list)
-    dl.addCallback(finished, client_list)
+    
+    dl.addCallback(trigger_rain_benchmark, client_list)
+    # dl.addCallback(finished, client_list)
 
 
 def shutdown_glassfish_rain(client_list, ret=None):
@@ -100,52 +116,67 @@ def phase_start_glassfish_database(done, client_list):
     the Rain Tracks of the SpecJ driver are accessing the Glassfish services. 
     """
     print 'starting glassfish and database...'
-    
-    dlist = []
-    
-    for target in hosts.get_hosts('target'):
-        print '   * starting glassfish on target %s' % (target) 
-        d = base.launch(client_list, target, 'glassfish_start', wait=False)
-        dlist.append(d)
+    try:
+        dlist = []
         
-        d = base.poll_for_message(client_list, target, 'glassfish_wait', 'domain1 running')
-        dlist.append(d)
-    
-    for target in hosts.get_hosts('database'):
-        print '   * starting database on target %s' % (target)
-        d = base.launch(client_list, target, 'spec_dbload')
-        dlist.append(d)
-    
-    # Wait for all drones to finish and set phase
-    dl = defer.DeferredList(dlist)
-    # dl.addCallback(phase_start_rain, client_list)
-    dl.addCallback(finished, client_list)
+        for target in hosts.get_hosts('target'):
+            print '   * starting glassfish on target %s' % (target) 
+            d = base.launch(client_list, target, 'glassfish_start', wait=False)
+            dlist.append(d)
+            
+            d = base.poll_for_message(client_list, target, 'glassfish_wait', 'domain1 running')
+            dlist.append(d)
+        
+        # Fill database in parallel
+#        for target in hosts.get_hosts('database'):
+#            print '   * initializing database on target %s' % (target)
+#            d = base.launch(client_list, target, 'spec_dbload')
+#            dlist.append(d)
+        
+        # Wait for all drones to finish and set phase
+        dl = defer.DeferredList(dlist)
+        
+        dl.addCallback(phase_start_rain, client_list)
+        # dl.addCallback(finished, client_list)
+    except Exception, e:
+        print e
+        finished(None, client_list)
  
  
 def phase_configure_glassfish(client_list):
     print 'reconfiguring glassfish ...'
-    
-    dlist = []
-    
-    for target in hosts.get_hosts('target'):
-        print '   * configuring glassfish on target %s' % (target)
+    try:
         
-        mysql_name = target.replace('glassfish', 'mysql')
-        print '     using mysql name: %s' % (mysql_name)
-        drones.prepare_drone('glassfish_configure', 'domain.xml', mysql_server=mysql_name)
-        drones.create_drone('glassfish_configure')
+        dlist = []
         
-        d = base.launch(client_list, target, 'glassfish_configure', wait=True)
-        dlist.append(d)
-    
-    # Wait for all drones to finish and set phase
-    dl = defer.DeferredList(dlist)
-    dl.addCallback(phase_start_glassfish_database, client_list)
+        for target in hosts.get_hosts('target'):
+            print '   * configuring glassfish on target %s' % (target)
+            
+            mysql_name = target.replace('glassfish', 'mysql')
+            print '     using mysql name: %s' % (mysql_name)
+            drones.prepare_drone('glassfish_configure', 'domain.xml', mysql_server=mysql_name)
+            drones.create_drone('glassfish_configure')
+            
+            d = base.launch(client_list, target, 'glassfish_configure', wait=True)
+            dlist.append(d)
+        
+        # Wait for all drones to finish and set phase
+        dl = defer.DeferredList(dlist)
+        
+        dl.addCallback(phase_start_glassfish_database, client_list)
+        # dl.addCallback(finished, client_list)
+        
+    except Exception, e:
+        print e
+        finished(None, client_list)
  
  
 def start_phase(client_list):
-    phase_configure_glassfish(client_list)
+    print 'Connection established'
+    print 'Following start sequence now...'
+    # phase_configure_glassfish(client_list)
     # phase_start_rain(None, client_list)
+    trigger_rain_benchmark(None, client_list)
     
     
 def stop_phase(client_list):
@@ -162,10 +193,13 @@ def main():
     # Create drones
     drones.main()
     
+    # Start or stop system
+    start = True
+    
     # Add hosts
-#    hosts.add_host('glassfish0', 'target')
-#    hosts.add_host('glassfish1', 'target')
-#    hosts.add_host('glassfish2', 'target')
+    hosts.add_host('glassfish0', 'target')
+    hosts.add_host('glassfish1', 'target')
+    hosts.add_host('glassfish2', 'target')
     hosts.add_host('glassfish3', 'target')
     hosts.add_host('glassfish4', 'target')
     hosts.add_host('glassfish5', 'target')
@@ -191,7 +225,6 @@ def main():
     wait.addErrback(errback)
     
     # Decide what to do after connection setup
-    start = True
     if start:
         print 'starting system ...'
         wait.addCallback(start_phase)
--- RelayControl/drones/rain_start/main.sh
+++ RelayControl/drones/rain_start/main.sh
@@ -9,5 +9,6 @@ cd /opt/rain/
 rm -rf /opt/rain/*.pid
 rm -rf /opt/rain/*.log
 
+
 # Launching Rain
 python start.py driver config/rain.config.specj.json > rain.log 2>&1 &
