2737d5b802fcdbbe3377dd8af595553888512ab9
Andreas Wolke <jacksonicson@gmail.com>
2012-08-26 13:54:55 +0200

    reversed to virt-top

--- sensors/virttop/main.sh
+++ sensors/virttop/main.sh
@@ -9,13 +9,21 @@ cleanup()
         kill $pid
 }
 
-trap cleanup SIGTERM
+trap "exit" SIGTERM
 
-top -b -d 0.5 | grep --line-buffered Cpu | sed -u 's/^.\{70\}[[:space:]]*//g' | sed -u 's/%st//' | \
+virt-top --script --stream | sed -u '{/virt-top.*/d}' | sed -u '{/ID.*/d}' | awk '{ printf "%s,%s\n",$7,$10; fflush() }' | \
 while read line ; do
-        ((res_timestamp = $(date +%s) ))
+        ((res_timestamp = $(date +%s)))
 
-        toPrint="$sensor,$res_timestamp,none,none,$line"
+        IIS=$IFS
+        IFS=","
+        tmp=($line)
+        IFS=$IIS
+
+        cpu=${tmp[0]}
+        domain=${tmp[1]}
+
+        toPrint="$sensor,$res_timestamp,none,$domain,$cpu"
         echo $toPrint
 done &
 
