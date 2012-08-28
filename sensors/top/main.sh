cba65bf68bcdc22119bcc51a0f10384b03ca18cc
Andreas Wolke <jacksonicson@gmail.com>
2012-08-28 10:58:57 +0200

    new version of the top command

--- sensors/top/main.sh
+++ sensors/top/main.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
 sensor=$1
-
-
 pid=-1
 
-cleanup()
+cleanup_term()
 {
-        kill $pid
+	# echo 'sigterm detected'
+	# echo "killing $pid"
+	pkill -TERM -P $$
+	exit 0
 }
 
-trap "exit" SIGTERM
+trap cleanup_term SIGTERM
 
-top -b -d 3 | grep --line-buffered Cpu | sed -u 's/^.\{70\}[[:space:]]*//g' | sed -u 's/%st//' | \
+flag=0
+set_flag()
+{
+	# echo 'setting flat'
+	flag=1
+}
+
+# grep --line-buffered Cpu | sed -u 's/^.\{70\}[[:space:]]*//g' | sed -u 's/%st//' |
+
+top -b -d 3 | grep --line-buffered Cpu | sed -u 's/^.\{70\}[[:space:]]*//g' | sed -u 's/%st//' | { 
+trap set_flag SIGTERM
 while read line ; do
-        ((res_timestamp = $(date +%s) ))
-        
+        (( res_timestamp = $(date +%s) ))
         toPrint="$sensor,$res_timestamp,none,none,$line"
-        echo $toPrint
-done &
+	echo $toPrint
+	if [ $flag -eq 1 ] ; then
+		# echo 'exiting'
+		break
+	fi
+done 
+# echo 'done'
+exit 0
+} &
 
 pid=$!
-
+echo $!
 wait
+exit 0
+
+
+
