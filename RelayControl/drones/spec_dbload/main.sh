e0c1b0d113919618419dfd1b45674d4a0b1752a7
Andreas Wolke <jacksonicson@gmail.com>
2012-08-31 15:58:35 +0200

    check if /mnt/share exists before creating it (database load)

--- RelayControl/drones/spec_dbload/main.sh
+++ RelayControl/drones/spec_dbload/main.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-mkdir /mnt/share
+if [ ! -d "/mnt/share" ]; then
+	mkdir /mnt/share
+fi
+
+# Mount NFS share with dumpfile
 mount -t nfs monitor0:/mnt/arr0/share /mnt/share
+
+# Load dumpfile
 mysql -uroot -prooter < /mnt/share/dumps/specdb\ txrate\ 60.sql
+
+# Umount NFS share
 umount /mnt/share
 
+# Print done
 echo "done"
