721fe6e1dee079099d749a54890bb6b7496f2d22
Andreas Wolke <jacksonicson@gmail.com>
2012-09-25 13:37:19 +0200

    fixed initial time setting in the browser

--- WebUI/static/browse.js
+++ WebUI/static/browse.js
@@ -55,19 +55,29 @@ function getSensors(){
 
 function setupDatePickers() {
 
-   	var myDate = new Date();
-    var month = myDate.getMonth() + 1;
-    var prettyDate = month + '/' + myDate.getDate() + '/' + myDate.getFullYear();
-	$("#startdate").val(prettyDate);
-	$("#stopdate").val(prettyDate);
-
+    var myDate = new Date();
+    myDate.setHours(myDate.getHours() - 5)
+    var prettyDate = (myDate.getMonth() + 1) + '/' + myDate.getDate() + '/' + myDate.getFullYear();
+    $("#startdate").val(prettyDate);
     $('#startdate').datepicker();
+
+    var prettyTime = myDate.getHours() + ':' + myDate.getMinutes();
+    $("#starttime").timepicker({
+        showMeridian:false,
+        defaultTime:prettyTime
+    });
+
+
+    var myDate = new Date();
+    myDate.setHours(myDate.getHours()+24)
+    var prettyDate = (myDate.getMonth() + 1) + '/' + myDate.getDate() + '/' + myDate.getFullYear();
+    $("#stopdate").val(prettyDate);
     $('#stopdate').datepicker();
 
-    $(document).ready(function () {
-        $('.timepicker-default').timepicker({
-            showMeridian: false
-        });
+    var prettyTime = myDate.getHours() + ':' + myDate.getMinutes();
+    $("#stoptime").timepicker({
+        showMeridian:false,
+        defaultTime:prettyTime
     });
 }
 
--- WebUI/static/logs.js
+++ WebUI/static/logs.js
@@ -6,24 +6,29 @@ $(function () {
 
 function setupDatePickers() {
 
+    var myDate = new Date();
+    myDate.setHours(myDate.getHours() - 5)
+    var prettyDate = (myDate.getMonth() + 1) + '/' + myDate.getDate() + '/' + myDate.getFullYear();
+    $("#startdate").val(prettyDate);
+    $('#startdate').datepicker();
 
-}
+    var prettyTime = myDate.getHours() + ':' + myDate.getMinutes();
+    $("#starttime").timepicker({
+        showMeridian:false,
+        defaultTime:prettyTime
+    });
 
-function setupDatePickers() {
 
     var myDate = new Date();
-    var month = myDate.getMonth() + 1;
-    var prettyDate = month + '/' + myDate.getDate() + '/' + myDate.getFullYear();
-    $("#startdate").val(prettyDate);
+    myDate.setHours(myDate.getHours()+24)
+    var prettyDate = (myDate.getMonth() + 1) + '/' + myDate.getDate() + '/' + myDate.getFullYear();
     $("#stopdate").val(prettyDate);
-
-    $('#startdate').datepicker();
     $('#stopdate').datepicker();
 
-    $(document).ready(function () {
-        $('.timepicker-default').timepicker({
-            showMeridian:false
-        });
+    var prettyTime = myDate.getHours() + ':' + myDate.getMinutes();
+    $("#stoptime").timepicker({
+        showMeridian:false,
+        defaultTime:prettyTime
     });
 
     getHostData(function (output) {
