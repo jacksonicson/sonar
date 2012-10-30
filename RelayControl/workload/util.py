874e0a19d4a614569bfaf4c1385adb6ac46f14c7
Andreas Wolke <jacksonicson@gmail.com>
2012-10-30 13:10:24 +0100

    refactoring profiles

--- RelayControl/workload/util.py
+++ RelayControl/workload/util.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 def to_array(timeSeries):
@@ -28,3 +29,24 @@ def to_array_collector(timeSeries, timeframe):
     demand = demand[0:iout]
         
     return time, demand
+
+
+def plot(ts, filename=None, threshold=1):
+    '''
+    Keyword arguments:
+    ts -- numpy array with the time series to plot
+    filename -- where to save the plot image to (default=None no plotting)
+    threshold -- set a limit on the y axis (default=1 for 100% CPU utilization)
+    '''
+    
+    fig = plt.figure()
+    
+    ax = fig.add_subplot(111)
+    ax.axis([0.0, len(ts), 0, threshold])
+    ax.plot(range(0, len(ts)), ts)
+    
+    if filename is not None: 
+        try:
+            plt.savefig('C:/temp/convolution/' + filename)
+        except:
+            pass 
