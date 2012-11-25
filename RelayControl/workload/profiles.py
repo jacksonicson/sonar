c6632ad1dbb74f1f778edcf8b5f00149dc06fdad
Andreas Wolke <jacksonicson@gmail.com>
2012-11-25 20:49:02 +0100

    benchmark configuration

--- RelayControl/workload/profiles.py
+++ RelayControl/workload/profiles.py
@@ -369,8 +369,8 @@ mix_sim = [
 ## CONFIGURATION            ##
 ##############################
 selected_profile = None # Prefix for picking TS from Times
-selected_name = 'mix_0' # Just for logging
-selected = mix_0        # Selected workload mix
+selected_name = 'mix_2' # Just for logging
+selected = mix_2        # Selected workload mix
 modified = False        # Modified version of the workload mix
 ##############################
 
