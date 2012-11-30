6bdad00391768d5f61f1d3895296dfc61e7243a5
Andreas Wolke <jacksonicson@gmail.com>
2012-11-30 11:04:03 +0100

    added behavior tree description

--- RelayControl/analytics/analytics.py
+++ RelayControl/analytics/analytics.py
@@ -27,7 +27,7 @@ TRACE_EXTRACT = False
 CONTROLLER_NODE = 'Andreas-PC'
 DRIVER_NODES = ['load0', 'load1']
 
-RAW = '29/11/2012 10:45:00    29/11/2012 17:35:00'
+RAW = '29/11/2012 17:45:00    30/11/2012 00:50:00'
 ##########################
 
 warns = []
