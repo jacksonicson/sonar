b2a46c2b84aa12c84e232f4fc1e22d089042eecc
Andreas Wolke <jacksonicson@gmail.com>
2013-01-05 20:31:19 +0100

    file based controller that reads an allocation matrix from a tex file

---
+++ RelayControl/control/logic/controller_file.py
@@ -0,0 +1,60 @@
+from logs import sonarlog
+from virtual import nodes, placement
+from control import domains
+import controller
+import json
+import configuration
+
+######################
+## CONFIGURATION    ##
+######################
+ALLOCATION_MATRIX_FILE = configuration.path('allocation360', 'txt')
+######################
+
+# Setup logging
+logger = sonarlog.getLogger('controller')
+
+class Sandpiper(controller.LoadBalancer):
+    
+    def __init__(self, pump, model):
+        super(Sandpiper, self).__init__(pump, model, 10 * 60, 120)
+        self.var = []
+        
+    def dump(self):
+        print 'Dump Sandpiper controller configuration...'
+        logger.info('Controller Configuration: %s' % json.dumps({'name' : 'File',
+                                                                 'allocation_matrix_file' : ALLOCATION_MATRIX_FILE,
+                                                                 }))
+    # Initial placement calculation (simulation only!!!)
+    def initial_placement_sim(self):
+        nodecount = len(nodes.HOSTS)
+        domaincount = len(domains.domain_profile_mapping)
+        
+        # Read allocation matrix
+        handle = open(ALLOCATION_MATRIX_FILE, 'r')
+        lines = handle.readlines()
+        handle.close()
+        
+        matrix = [[0 in xrange(domaincount)] in xrange(nodecount)]
+        for x, line in enumerate(lines):
+            elements = line.split(',')
+            for y, element in enumerate(elements):
+                matrix[x][y] = int(element)
+                
+        # Convert allocation matrix to a migration list
+        migrations = []
+        for y in xrange(len(matrix)):
+            for x in xrange(len(matrix[x])):
+                if x > 0:
+                    mapping = domains.domain_profile_mapping[x]
+                    migration = (mapping.domain, y)
+                    migrations.append(migration)
+        
+        # Build internal model 
+        self.build_internal_model(migrations)
+        return migrations
+    
+    def balance(self):
+        # print 'SSAPv static - not controlling'
+        pass
+    
