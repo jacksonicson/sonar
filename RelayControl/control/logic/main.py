3ee0e7ce80ec2615a9ff583c1d138b509271a6bf
Andreas Wolke <jacksonicson@gmail.com>
2013-01-09 09:53:29 +0100

    simulations

--- RelayControl/control/logic/main.py
+++ RelayControl/control/logic/main.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
         name = '%s - %s - %s' % (mix, controller, postfix)
         t = open(config.path(name), 'w')
         buffer = []
-        for i in xrange(0, 5):
+        for i in xrange(0, 15):
             domains.recreate()
             pump = main(controller)
             res = scoreboard.Scoreboard().get_result_line(pump)
