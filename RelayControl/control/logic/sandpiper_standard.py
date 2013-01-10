886aa9cb827fe36d32bb5d04329ff4e3f121d348
johanneskross <johanneskross@gmail.com>
2013-01-10 19:13:47 +0100

    reactive controller in extra python module

--- RelayControl/control/logic/sandpiper_standard.py
+++ RelayControl/control/logic/sandpiper_standard.py
@@ -1,11 +1,10 @@
 from logs import sonarlog
-from model import types
 import json
 import controller
-import util
 import placement
 import migration_scheduler
 import controller_imbalance
+import controller_reactive
 import controller_swap
 from virtual import nodes
 
@@ -25,9 +24,8 @@ M_VALUE = 17 # m values out of the window k must be above or below the threshold
 
 # MIXED CONTROLLER SETTINGS
 # Values can be 'imbalance', 'reactive', 'swap' or ''
-# Notice: Swap cannot be executed before 'reactive'
 CONTROLLER_SETTINGS = {
-'first_controller' : 'swap',
+'first_controller' : 'reactive',
 'second_controller' : '',
 'third_controller' : ''
 }
@@ -100,6 +98,7 @@ class Sandpiper(controller.LoadBalancer):
     def balance(self):
         sleep_time = 60
         time_now = self.pump.sim_time()
+        
         self.migration_triggered = False
         
         self.controller_setup['first_controller'](time_now, sleep_time, K_VALUE)
@@ -118,14 +117,8 @@ class Sandpiper(controller.LoadBalancer):
 
     
     def reactive_controller(self, time_now, sleep_time, K_VALUE):
-        # detect hotspots
-        self.hotspot_detector()
-            
-        # calculate and sort nodes by their volume
-        nodes = self.migration_manager()
-        
-        # trigger migration
-        self.migration_trigger(nodes, sleep_time, time_now)
+        reactive_controller = controller_reactive.Reactive(self, THRESHOLD_OVERLOAD, THRESHOLD_UNDERLOAD, PERCENTILE, K_VALUE, M_VALUE)
+        reactive_controller.migrate_reactive(time_now, sleep_time)
         
     def swap_controller(self, time_now, sleep_time, K_VALUE):
         swap_controller = controller_swap.Swap(self, THRESHOLD_OVERLOAD, THRESHOLD_UNDERLOAD, PERCENTILE, K_VALUE, M_VALUE)
@@ -135,142 +128,3 @@ class Sandpiper(controller.LoadBalancer):
         # DO NOTHING
         return
     
-    def hotspot_detector(self):
-        ############################################
-        ## HOTSPOT DETECTOR ########################
-        ############################################ 
-        for node in self.model.get_hosts(types.NODE):
-            # Check past readings
-            readings = node.get_readings()
-            
-            # m out of the k last measurements are used to detect overloads 
-            k = K_VALUE
-            overload = 0
-            underload = 0
-            for reading in readings[-k:]:
-                if reading > THRESHOLD_OVERLOAD: overload += 1
-                if reading < THRESHOLD_UNDERLOAD: underload += 1
-
-            m = M_VALUE   
-            overload = (overload >= m)
-            underload = (underload >= m)
-             
-            if overload:
-                print 'Overload in %s - %s' % (node.name, readings[-k:])  
-             
-            # Update overload                                
-            node.overloaded = overload
-            node.underloaded = underload
-          
-    def migration_manager(self):
-        ############################################
-        ## MIGRATION MANAGER #######################
-        ############################################
-        # Calculate volumes of each node
-        nodes = []
-        domains = []
-        for node in self.model.get_hosts():
-            node = self.volume(node, K_VALUE)
-            
-            if node.type == types.NODE:
-                nodes.append(node)
-            elif node.type == types.DOMAIN: 
-                domains.append(node)
-       
-        # Sort nodes to their volume in DECREASING order
-        # Multiplication with a big value to shift post comma digits to the front (integer)
-        nodes.sort(lambda a, b: int((b.volume - a.volume) * 100000))
-        
-        return nodes 
-    
-    def migration_trigger(self, nodes, sleep_time, time_now):
-        ############################################
-        ## MIGRATION TRIGGER #######################
-        ############################################
-        for node in nodes:
-            node.dump()
-            
-            # Overload situation
-            try:
-                if node.overloaded:
-                    # Source node to migrate from 
-                    source = node
-                    
-                    # Sort domains by their VSR value in decreasing order 
-                    node_domains = []
-                    node_domains.extend(node.domains.values())
-                    node_domains.sort(lambda a, b: int(b.volume_size - a.volume_size))
-                    
-                    # Try to migrate all domains by decreasing VSR value
-                    for domain in node_domains:
-                        self.migrate_overload(node, nodes, source, domain, time_now, sleep_time, K_VALUE, False)
-                        self.migrate_overload(node, nodes, source, domain, time_now, sleep_time, K_VALUE, True)
-                            
-            except StopIteration: pass 
-            
-            # Underload situation
-            try:
-                if node.underloaded:
-                    # Source node to migrate from 
-                    source = node
-                    
-                    # Sort domains by their VSR value in decreasing order 
-                    node_domains = []
-                    node_domains.extend(node.domains.values())
-                    node_domains.sort(lambda a, b: int(b.volume_size - a.volume_size))
-                    
-                    # Try to migrate all domains by decreasing VSR value
-                    for domain in node_domains:
-                        self.migrate_underload(node, nodes, source, domain, time_now, sleep_time, K_VALUE, False)
-                        self.migrate_underload(node, nodes, source, domain, time_now, sleep_time, K_VALUE, True)
-                            
-            except StopIteration: pass
-    
-    def migrate_overload(self, node, nodes, source, domain, time_now, sleep_time, k, empty):
-        # Try all targets for the migration (reversed - starting at the BOTTOM)
-        for target in reversed(range(nodes.index(node) + 1, len(nodes))):
-            target = nodes[target]
-               
-            if len(target.domains) == 0 and empty == False:
-                continue
-                             
-            test = True
-            test &= (target.percentile_load(PERCENTILE, k) + util.domain_to_server_cpu(target, domain, domain.percentile_load(PERCENTILE, k))) < THRESHOLD_OVERLOAD # Overload threshold
-            test &= len(target.domains) < 6
-            test &= (time_now - target.blocked) > sleep_time
-            test &= (time_now - source.blocked) > sleep_time
-                            
-            if test: 
-                migration_type = 'Overload (Empty=%s)' % (empty)
-                self.migration_scheduler.add_migration(domain, source, target, migration_type) 
-                self.migration_triggered = True
-                raise StopIteration()
-        
-    def migrate_underload(self, node, nodes, source, domain, time_now, sleep_time, k, empty):
-        # Try all targets for the migration
-        for target in range(nodes.index(node) - 1):
-            target = nodes[target]
-            
-            if len(target.domains) == 0 and empty == False:
-                continue
-            
-            test = True
-            test &= (target.percentile_load(PERCENTILE, k) + util.domain_to_server_cpu(target, domain, domain.percentile_load(PERCENTILE, k))) < THRESHOLD_OVERLOAD # Overload threshold
-            test &= len(target.domains) < 6
-            test &= (time_now - target.blocked) > sleep_time
-            test &= (time_now - source.blocked) > sleep_time
-            
-            if test: 
-                migration_type = 'Underload (Empty=%s)' % (empty)
-                self.migration_scheduler.add_migration(domain, source, target, migration_type)                          
-                self.migration_triggered = True
-                raise StopIteration()
-
-    def volume(self, node, k):
-        # Calculates volume for node and return node
-        volume = 1.0 / max(0.001, float(100.0 - node.percentile_load(PERCENTILE, k)) / 100.0)
-        node.volume = volume
-        node.volume_size = volume / 8.0 # 8 GByte
-        
-        return node    
-            
